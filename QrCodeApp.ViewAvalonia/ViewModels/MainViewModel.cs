81565f830f66198562581d384d1f04201ff4c0ad
zorindaniilleon <daniil.zorin.leon@gmail.com>
2023-09-04 01:43:37 +0700

    fix

--- QrCodeApp.ViewAvalonia/ViewModels/MainViewModel.cs
+++ QrCodeApp.ViewAvalonia/ViewModels/MainViewModel.cs
@@ -1,6 +1,27 @@
-﻿namespace QrCodeApp.ViewAvalonia.ViewModels;
+﻿using ReactiveUI;
+using System.Reactive;
+
+namespace QrCodeApp.ViewAvalonia.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private string? _qrCodeText;
+
+    public ReactiveCommand<Unit, Unit> CreateQrCodeCommand { get; }
+
+    public MainViewModel()
+    {
+        CreateQrCodeCommand = ReactiveCommand.Create(CreateQrCode);
+    }
+
+    public string? QrCodeText
+    {
+        get => _qrCodeText;
+        set => _qrCodeText = this.RaiseAndSetIfChanged(ref _qrCodeText, value);
+    }
+
+    public void CreateQrCode()
+    {
+
+    }
 }
